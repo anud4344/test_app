b8f709ef63c4fd05ba52b5381a863a60d0acd7d1
Anuraag Deepak <d.anuraag@tamu.edu>
2025-09-17 05:41:37 +0000

    Added seeds.rb and modified index.html

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,36 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Book.create!([
-   {title: "The life of a turtle"},
-   {title: "The mirror, the lion and the audacity of the little dog"},
-   {title: "Origami for cats"},
-   {title: "The weight of a hundred kilograms"}
+ case Rails.env
+
+ when "development"
+    puts "Seeding DEVELOPMENT.."
+    Book.destroy_all
+    Book.create!([
+        {title: "The life of a turtle"},
+        {title: "The mirror, the lion and the audacity of the little dog"},
+        {title: "Origami for cats"},
+        {title: "The weight of a hundred kilograms"}
 ])
+
+ when "test"
+    puts "Seeding TEST.."
+    Book.destroy_all
+    Book.create!([
+        {title: "Life of Pi"},
+        {title: "The Bible"},
+        {title: "Gifted Hands"},
+        {title: "Harry Potter"}
+])
+ 
+when "production"
+    puts "Seeding PRODUCTION.."
+    Book.destroy_all
+    Book.create!([
+        {title: "Life of Mahatma Gandhi"},
+        {title: "1984"},
+        {title: "Tintin"},
+        {title: "National Geographic Magazine"}
+])
+
+end
